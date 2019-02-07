7031f9b66d68927cc3558a381d96af0088e5b260
Hampton Catlin <hcatlin@gmail.com>
2019-02-07 11:43:36 -0500

    add sass and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from './Button';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./Button";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <header className="App-Header">
-            JENN'S WARDROBE
-          </header>
-          <div className="Content">
-            <div className="Content-Left">
-              <Button 
-                text="AUTO DRESS"
-                width={170}
-                height={95} 
-              />
-            </div>
-            <div className="Content-Center">
-            </div>
-            <div className="Content-Right">
-              <Button 
-                  text="DRESS ME"
-                  width={170}
-                  height={95} 
-                />
-            </div>
+        <header className="App-Header">JENN'S WARDROBE</header>
+        <div className="Content">
+          <div className="Content-Left">
+            <Button
+              text="AUTO DRESS"
+              width={170}
+              height={95}
+              activated={true}
+            />
           </div>
-          <footer className="App-Footer">
-
-          </footer>
+          <div className="Content-Center" />
+          <div className="Content-Right">
+            <Button text="DRESS ME" width={170} height={95} />
+          </div>
+        </div>
+        <footer className="App-Footer" />
       </div>
     );
   }
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import "./Button.css";
+import React from "react";
+import "./Button.scss";
 
-const Button = (props: { 
-    text: String,
-    width?: number,
-    height?: number,
-    activated?: true
+const Button = (props: {
+  text: String;
+  width?: number;
+  height?: number;
+  activated?: boolean;
 }) => {
-    let className = "Button"
-    if(props.activated === true) {
-        className += " Active"
-    }
-    return (
-        <div className={"Button"} style={{
-            width: props.width || 100,
-            height: props.height || 75,
-        }}>
-            <div className="Button-Inner">
-              <p className="Text">{props.text}</p>
-            </div>
-        </div>
-    );
+  let className = "Button";
+  if (props.activated) {
+    className += " Active";
+  }
+  return (
+    <div
+      className={className}
+      style={{
+        width: props.width || 100,
+        height: props.height || 75
+      }}
+    >
+      <div className="Button-Inner">
+        <p className="Text">{props.text}</p>
+      </div>
+    </div>
+  );
 };
-
-
 
 export default Button;
